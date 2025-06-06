d4cc90f9ac4d7d501d9f0e26dff0b0e37e158eb9
Vespid90 <farid.elmam@gmail.com>
2025-06-06 16:15:55 +0200

    structure project

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "budget_manager",
+
+    'summary': "Gestionnaire de budget pour entreprise",
+
+    'description': """
+Gestionnaire de budget pour entreprise
+    """,
+
+    'author': "Vespid",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Budget manager',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mail'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/budget_planner_view.xml',
+        'views/budget_line_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}

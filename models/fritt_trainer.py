573874255a495d64c51a218ef4d31fadf66c0bf1
Rafael Pili <rafaelpili4@gmail.com>
2025-06-10 15:31:59 +0200

    bouton planned lessons

--- models/fritt_trainer.py
+++ models/fritt_trainer.py
@@ -14,5 +14,23 @@ class FrittTrainer(models.Model):
         ('soccer', "Soccer"),
     ], string="Specialty")
     image = fields.Binary(string="Image")
+    planned_lesson_ids = fields.One2many(comodel_name="fritt.group.lesson", inverse_name="trainer_id", string="Planned Lessons")
+    planned_lesson_count = fields.Integer(string="Planned Lessons", compute="_compute_planned_lesson")
 
 
+
+    @api.depends('planned_lesson_ids')
+    def _compute_planned_lesson(self):
+        for record in self:
+            record.planned_lesson_count= len(record.planned_lesson_ids)
+
+    def open_planned_lesson(self):
+        action = {
+            'name': 'open_planned_lesson',
+            'type': 'ir.actions.act_window',
+            'res_model': 'fritt.group.lesson',
+            'view_mode': 'kanban,list,form',
+            'domain': [('trainer_id', '=', self.name)]
+        }
+
+        return action

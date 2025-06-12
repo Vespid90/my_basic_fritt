6fa06d7a3dab9c953d5ede0eb4342b23c94bc4d1
Vespid90 <farid.elmam@gmail.com>
2025-06-12 15:24:40 +0200

    refacto and good practice python done

--- models/fritt_subscription.py
+++ models/fritt_subscription.py
@@ -8,18 +8,24 @@ class FrittSubscription(models.Model):
     _description = 'Subscription'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
+    #Boolean
     active = fields.Boolean(string='Active', default=True)
 
-    name = fields.Char(string='Subscription name', required=True)#translate=true ; à voir comment l'intégrer
+    #Char
+    name = fields.Char(string='Subscription name', required=True, translate=True)
     description = fields.Char(string="Member's subscription")
 
+    #Datetime
     date_today = fields.Datetime()
 
-    price = fields.Monetary(string='Price', currency_field='currency_id')
+    #Monetary
+    price = fields.Monetary(string='Price', currency_field='currency_id', compute="_compute_price_access")
 
+    #Integer
     time = fields.Integer(string='Subscription time remaining')
     sequence = fields.Integer(default=10)
 
+    #Selection
     type = fields.Selection(
         selection=[('basic', 'Basic'),
                    ('premium', 'Premium'),
@@ -29,56 +35,27 @@ class FrittSubscription(models.Model):
         selection=[('10', '10 days per month'),
                    ('30', '30 days per month'),
                    ('illimited', 'Illimited')],
-        string="Time's access")
+        string="Time's access", compute="_compute_price_access")
 
-    # relations
+    #relations
     currency_id = fields.Many2one(comodel_name='res.currency', string='Devise',
                                   default=lambda self: self.env.company.currency_id)
     member_ids = fields.One2many(comodel_name='fritt.member', inverse_name='subscription_id', string='Members name')
 
-    # logique
-    @api.onchange('type')  # changement visuel lors de la creation d'abonnements
-    def _onchange_type(self):
-        match self.type:
-            case 'basic':
-                self.time_access = '10'
-                self.price = 10
-            case 'premium':
-                self.time_access = '30'
-                self.price = 30
-            case 'vip':
-                self.time_access = 'illimited'
-                self.price = 60
-
-    @api.model_create_multi
-    def create(self, vals_list):  # méthode create pour enregistrer les abonnements dans la db
-        for vals in vals_list:
-            if not vals.get('type'):
-                raise ValidationError("Please choose an subscription type")
-
-        for res in vals_list:
-            match res.get('type'):  # match/case pour faire concorder les données de chaque abonnements
+    #logique
+    @api.depends('type')
+    def _compute_price_access(self):
+        for rec in self:
+            match rec.type:
                 case 'basic':
-                    res['time_access'] = '10'
-                    res['price'] = 10
+                    rec.time_access = '10'
+                    rec.price = 10
                 case 'premium':
-                    res['time_access'] = '30'
-                    res['price'] = 30
+                    rec.time_access = '30'
+                    rec.price = 30
                 case 'vip':
-                    res['time_access'] = 'illimited'
-                    res['price'] = 60
-        results = super().create(vals_list)
-        return results
-
-    def write(self, vals):  # méthode write pour enregistrer les éditions d'abonnements dans la db
-        match vals.get('type'):
-            case 'basic':
-                vals['time_access'] = '10'
-                vals['price'] = 10
-            case 'premium':
-                vals['time_access'] = '30'
-                vals['price'] = 30
-            case 'vip':
-                vals['time_access'] = 'illimited'
-                vals['price'] = 60
-        return super().write(vals)
+                    rec.time_access = 'illimited'
+                    rec.price = 60
+                case _:
+                    rec.time_access = ''
+                    rec.price = 0
